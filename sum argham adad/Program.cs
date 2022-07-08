b1df0809c4f030cd68a1043012fd5788d3f07cc6
Yamahdi@1984 <fatemeh.kamali198422@gmail.com>
2022-07-09 02:34:39 +0430

    tamrin8

---
+++ sum argham adad/Program.cs
@@ -0,0 +1,12 @@
+﻿int num, a, sum=0;
+System.Console.WriteLine("Enter a number:");
+num=int.Parse(Console.ReadLine());
+while (num != 0)
+{
+    a = num % 10 ;
+    num = num / 10 ;
+    sum = sum + a ;
+}
+
+System.Console.WriteLine("sum argham adad=" + sum);
+ 
